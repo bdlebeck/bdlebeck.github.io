95238be7a95a6b3844c0413033497e765c76d7b5
Brian Lebeck <bdlebeck@gmail.com>
2019-07-30 23:08:00 -0700

    Updated details view.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
 
 	$.get('templates/skills.html', function(skillTemplate) {
 		var skillOutput = Mustache.render(skillTemplate, skill);
-		$('#section-skills').html(skillOutput);
+		$('#skills').html(skillOutput);
 	});	
 
 	$.get('templates/careers.html', function(careerTemplate) {
 		var careerOutput = Mustache.render(careerTemplate, career);
-		$('#section-career').html(careerOutput);
+		$('#career').html(careerOutput);
 	});
 
 	$.get('templates/projects.html', function(projectTemplate) {
 		var projectOutput = Mustache.render(projectTemplate, project);
-		$('#section-projects').html(projectOutput);
+		$('#projects').html(projectOutput);
 	});	
 
 	$.get('templates/education.html', function(educationTemplate) {
 		var educationOutput = Mustache.render(educationTemplate, education);
-		$('#section-education').html(educationOutput);
+		$('#education').html(educationOutput);
 	});		
 
 
