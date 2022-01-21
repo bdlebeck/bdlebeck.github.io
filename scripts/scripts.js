3a9c450007792cf8cbff692fad53a96dc30ab4ea
Brian Lebeck <bdlebeck@gmail.com>
2022-01-20 18:34:26 -0800

    UI Update, responsive, breakpoints, alignment.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
 	$.get('templates/skills.html', function(strengthTemplate) {
-		var strengthOutput = Mustache.render(strengthTemplate, strength);
-		$('#strengths').html(strengthOutput);
+		var strengthOutput = Mustache.render(strengthTemplate);
+		$('#strength').html(strengthOutput);
 	});	
+
+	$.get('templates/skills2.html', function(strength2Template) {
+		var strength2Output = Mustache.render(strength2Template);
+		$('#strength2').html(strength2Output);
+	});		
 
 	$.get('templates/careers.html', function(careerTemplate) {
 		var careerOutput = Mustache.render(careerTemplate, career);
